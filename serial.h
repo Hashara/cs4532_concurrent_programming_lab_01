47720d61f95ea908d0f181a356eecfd297600a9b
Hashara <kumarasinghe.k.a.s.h@gmail.com>
2021-07-28 11:24:51 +0530

    serial run done

---
+++ serial.h
@@ -0,0 +1,12 @@
+#ifndef LAB_SERIAL_H
+#define LAB_SERIAL_H
+
+#include <stdio.h>
+#include <time.h>
+
+#include "linked_list.h"
+#define MAX 65535
+
+void serial_run(float m_member, float m_insert, float  m_delete, struct list_node** head, int m );
+
+#endif //LAB_SERIAL_H
