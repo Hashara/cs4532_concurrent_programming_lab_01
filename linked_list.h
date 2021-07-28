00ffd87446e5a40c51f45824647145bd36424aea
Shammi Kolonne <shammi0107@gmail.com>
2021-07-28 07:05:34 +0530

    link list created

---
+++ linked_list.h
@@ -0,0 +1,19 @@
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+struct list_node {
+    int data;
+    struct list_node * next;
+};
+
+int Member(int value, struct list_node *head_p);
+int Insert(int value, struct list_node **head_p);
+int Delete(int value, struct list_node **head_p);
+void Print(struct list_node * head_p);
+//void destructor(struct list_node* head_p);
+
+#endif //LINKED_LIST_H
--- main.c
+++ main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <time.h>
+#include "linked_list.h"
 
 int main() {
+    const int max_val = 65535;
+    const int elementsNum = 10;
+    int count = 0;
+    struct list_node *head = NULL;
+
+    srand(time(0));
+    while (count < elementsNum) {
+        int val = rand() % max_val;
+        Insert(val, &head);
+        count++;
+    }
+    Insert(20000, &head);
+    Print(head);
+
+//    Delete(20000, &head);
+//    Print(head);
+
+//    destructor(&head);
     printf("Hello, World!\n");
-    return 0;
+    Member(20000, head);
+    Member(200000, head);
 }
